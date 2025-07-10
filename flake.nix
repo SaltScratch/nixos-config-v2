d3b88dfc477cffedafbeab367c1a7d441111e00d
Allan Cairns <allan@cairns.id.au>
2025-07-10 20:28:54 +0800

    enable home-manager

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    # NOTE: 'nixos' is the default hostname set by the installer
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -13,6 +14,12 @@
           nix = {
             settings.experimental-features = [ "nix-command" "flakes" ];
           };
+        }
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.allan = import ./home.nix;
         }
       ];
     };
