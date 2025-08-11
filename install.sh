ad9ac0c5f9ef963111b4e1ec5e03fea70d596c24
Allan Cairns <allan@cairns.id.au>
2025-08-11 08:25:18 +0100

    remove hardware config and use the one in /etc/nixos

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ git clone https://github.com/giddisey/nixos-config-v2.git ~/nixos-config-v2
 cd ~/nixos-config-v2 || exit
 
 # copy hardware config
-sudo cp -f /etc/nixos/hardware-configuration.nix .
-sudo chown $USER ./hardware-configuration.nix
-git add -N .
+# sudo cp -f /etc/nixos/hardware-configuration.nix .
+# sudo chown $USER ./hardware-configuration.nix
+# git add -N .
 
 # generate keybinds cheatsheet
 sh ./waybar/generate-keybinds.sh
 
 # rebuild/switch
-sudo nixos-rebuild switch --flake .
+sudo nixos-rebuild switch --flake . --impure
--- rebuild-switch.sh
+++ rebuild-switch.sh
@@ -1,4 +1,4 @@
 # generate keybinds cheatsheet
 sh ~/nixos-config-v2/waybar/generate-keybinds.sh
 
-sudo nixos-rebuild switch --flake ~/nixos-config-v2
+sudo nixos-rebuild switch --flake ~/nixos-config-v2 --impure
